82c75f90779cdd550513540616bbd561b47cf18f
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-08-31 14:42:04 +0900

    [류지수] - 최초 파일 추가 - yml을 통한 database 연동 - jsp 관련 build.gradle 수정

---
+++ src/main/java/com/nunbub/marondalgram/MarondalgramApplication.java
@@ -0,0 +1,13 @@
+package com.nunbub.marondalgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarondalgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarondalgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nunbub/marondalgram/MarondalgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunbub.marondalgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarondalgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
